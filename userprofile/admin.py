48e2ce110a724ae611ea7e0ef0333bd665dccf6d
Zhongqi Sun <zhongqi.sun@gmail.com>
2016-03-22 17:22:21 -0700

    Add user profile to admin

--- userprofile/admin.py
+++ userprofile/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+#from userprofile.models import UserRole, bizCategory, Profile,investorType, capitalType, InvestorProfile, companyType,fundingType, CompanyProfile
+from userprofile.models import *
 # Register your models here.
+admin.site.register(UserRole)
+admin.site.register(bizCategory)
+admin.site.register(Profile)
+admin.site.register(investorType)
+admin.site.register(capitalType)
+admin.site.register(InvestorProfile)
+admin.site.register(companyType)
+admin.site.register(fundingType)
+admin.site.register(CompanyProfile)
